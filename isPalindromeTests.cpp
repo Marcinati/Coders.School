1ae7cbdcc076b47ef1f016bb97acb00b10fd25b4
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-11 19:02:32 +0200

    Update isPalindromeTests.cpp

--- isPalindromeTests.cpp
+++ isPalindromeTests.cpp
@@ -3,21 +3,18 @@
 
 bool isPalindrome(const std::string & text)
 {
-    int difference=0;
+    bool difference=true;
     
     for (int i=1; i <= (text.length()/2); i++)
     {
         if (tolower(text[i-1]) != tolower(text[text.length()-i]) || text[i-1] == '\32')
         {
             return false;
-            difference++;
+            difference=false;
         }   
     }
-   
-    if (difference == 0)
-    {
-        return true;
-    }
+  
+    return true;
 }
 
 int main()
